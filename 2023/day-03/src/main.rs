353fbc8af86ccd1b8efba4101f58ddab0693e430
skybax <tmflame@gmail.com>
2023-12-04 18:16:03 -0600

    Day 3 success!

--- 2023/day-03/src/main.rs
+++ 2023/day-03/src/main.rs
@@ -7,6 +7,7 @@ struct NumberToken(usize, usize);
 struct LineData {
     number_indexes: Vec<NumberToken>,
     symbol_indexes: Vec<usize>,
+    gear_indexes: Vec<usize>,
     text: String,
 }
 
@@ -15,6 +16,7 @@ impl LineData {
         LineData {
             number_indexes: Vec::new(),
             symbol_indexes: Vec::new(),
+            gear_indexes: Vec::new(),
             text: String::new(),
         }
     }
@@ -52,6 +54,9 @@ fn read_input() -> Vec<LineData> {
                 in_number = false;
                 if c != '.' {
                     line_data.symbol_indexes.push(i);
+                    if c == '*' {
+                        line_data.gear_indexes.push(i);
+                    }
                 }
             }
         }
@@ -70,8 +75,8 @@ fn part1(data: &Vec<LineData>) {
             // Valid from same line
             for symbol_index in &current_line_data.symbol_indexes {
                 let owned_symbol = symbol_index.to_owned();
-                if number_index.0 > 0
-                    && owned_symbol >= number_index.0 - 1
+                if ((number_index.0 > 0 && owned_symbol >= number_index.0 - 1)
+                    || owned_symbol >= number_index.0)
                     && owned_symbol <= number_index.1 + 1
                 {
                     let valid_value = current_line_data.text[number_index.0..number_index.1 + 1]
@@ -87,8 +92,8 @@ fn part1(data: &Vec<LineData>) {
                 let prev_line_data = data.get(i - 1).expect("Error getting previous line data");
                 for symbol_index in &prev_line_data.symbol_indexes {
                     let owned_symbol = symbol_index.to_owned();
-                    if number_index.0 > 0
-                        && owned_symbol >= number_index.0 - 1
+                    if ((number_index.0 > 0 && owned_symbol >= number_index.0 - 1)
+                        || owned_symbol >= number_index.0)
                         && owned_symbol <= number_index.1 + 1
                     {
                         let valid_value = current_line_data.text
@@ -106,8 +111,8 @@ fn part1(data: &Vec<LineData>) {
                 let next_line_data = data.get(i + 1).expect("Error getting next line data");
                 for symbol_index in &next_line_data.symbol_indexes {
                     let owned_symbol = symbol_index.to_owned();
-                    if number_index.0 > 0
-                        && owned_symbol >= number_index.0 - 1
+                    if ((number_index.0 > 0 && owned_symbol >= number_index.0 - 1)
+                        || owned_symbol >= number_index.0)
                         && owned_symbol <= number_index.1 + 1
                     {
                         let valid_value = current_line_data.text
@@ -125,4 +130,65 @@ fn part1(data: &Vec<LineData>) {
     println!("Part 1: {}", total_value);
 }
 
-fn part2(data: &Vec<LineData>) {}
+fn part2(data: &Vec<LineData>) {
+    let mut total_value: i32 = 0;
+
+    for (i, line_data) in data.iter().enumerate() {
+        for gear_index in &line_data.gear_indexes {
+            let mut gear_ratios: Vec<i32> = Vec::new();
+
+            // collect numbers from current line
+            for number_index in &line_data.number_indexes {
+                if (*gear_index > 0 && *gear_index - 1 == number_index.1)
+                    || *gear_index + 1 == number_index.0
+                {
+                    let gear_value = line_data.text[number_index.0..number_index.1 + 1]
+                        .parse::<i32>()
+                        .expect("Error parsing integer from string");
+                    gear_ratios.push(gear_value);
+                }
+            }
+
+            // collect numbers from prev line
+            if i > 0 {
+                let prev_line_data = data.get(i - 1).expect("Error getting prev line");
+                for number_index in &prev_line_data.number_indexes {
+                    if ((number_index.0 > 0 && *gear_index >= number_index.0 - 1)
+                        || *gear_index >= number_index.0)
+                        && *gear_index <= number_index.1 + 1
+                    {
+                        let gear_value = prev_line_data.text[number_index.0..number_index.1 + 1]
+                            .parse::<i32>()
+                            .expect("Error parsing integer from string");
+                        gear_ratios.push(gear_value);
+                    }
+                }
+            }
+
+            // collect numbers from next line
+            if i + 1 < data.len() {
+                let next_line_data = data.get(i + 1).expect("Error getting next line");
+                for number_index in &next_line_data.number_indexes {
+                    if ((number_index.0 > 0 && *gear_index >= number_index.0 - 1)
+                        || *gear_index >= number_index.0)
+                        && *gear_index <= number_index.1 + 1
+                    {
+                        let gear_value = next_line_data.text[number_index.0..number_index.1 + 1]
+                            .parse::<i32>()
+                            .expect("Error parsing integer from string");
+                        gear_ratios.push(gear_value);
+                    }
+                }
+            }
+
+            if gear_ratios.len() > 1 {
+                let mut gear_ratio = gear_ratios.pop().expect("Error popping element");
+                for gr in gear_ratios {
+                    gear_ratio *= gr;
+                }
+                total_value += gear_ratio;
+            }
+        }
+    }
+    println!("Part 2: {}", total_value);
+}
