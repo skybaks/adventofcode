c81466a193a34e80e0db80340d64f96155b9bcc1
skybax <tmflame@gmail.com>
2023-12-07 18:31:57 -0600

    Part 2! Finally got the joker code working

--- 2023/day-07/src/main.rs
+++ 2023/day-07/src/main.rs
@@ -4,7 +4,6 @@ use std::fs;
 struct CamelHand {
     class: i64,
     hand_nums: [i64; 5],
-    hand_str: String,
     bid: i64,
 }
 
@@ -27,63 +26,91 @@ fn card_to_num(card: &char) -> i64 {
     }
 }
 
+fn card_to_num_joker(card: &char) -> i64 {
+    match card {
+        'J' => 1,
+        _ => card_to_num(card),
+    }
+}
+
 fn classify_hand(hand: &[i64; 5]) -> i64 {
     let mut card_counts: Vec<usize> = Vec::new();
     card_counts.reserve(13);
     for i in 2..15 {
-        let instances = hand.into_iter().filter(|&v| *v == i).count();
+        let instances = hand.iter().filter(|&v| *v == i).count();
         card_counts.push(instances);
     }
+    let num_jokers = hand.iter().filter(|&v| *v == 1).count();
 
-    if card_counts.contains(&5) {
-        //println!("Five of a kind {:?}", hand);
+    if card_counts.contains(&5)
+        || (num_jokers == 5)
+        || (num_jokers == 4 && card_counts.contains(&1))
+        || (num_jokers == 3 && card_counts.contains(&2))
+        || (num_jokers == 2 && card_counts.contains(&3))
+        || (num_jokers == 1 && card_counts.contains(&4))
+    {
         return 7;
-    } else if card_counts.contains(&4) {
-        //println!("Four of a kind {:?}", hand);
+    } else if card_counts.contains(&4)
+        || (num_jokers == 4)
+        || (num_jokers == 3 && card_counts.contains(&1))
+        || (num_jokers == 2 && card_counts.contains(&2))
+        || (num_jokers == 1 && card_counts.contains(&3))
+    {
         return 6;
-    } else if card_counts.contains(&3) {
-        if card_counts.contains(&2) {
-            //println!("Full house {:?}", hand);
+    } else if card_counts.contains(&3)
+        || (num_jokers == 3)
+        || (num_jokers == 2 && card_counts.contains(&1))
+        || (num_jokers == 1 && card_counts.contains(&2))
+    {
+        if (card_counts.contains(&3) && card_counts.contains(&2))
+            || (num_jokers == 3 && card_counts.contains(&2))
+            || (num_jokers == 2 && card_counts.contains(&1) && card_counts.contains(&2))
+            || (num_jokers == 1 && card_counts.iter().filter(|&v| *v == 2).count() == 2)
+        {
             return 5;
         } else {
-            //println!("Three of a kind {:?}", hand);
             return 4;
         }
-    } else if card_counts.contains(&2) {
-        if card_counts.into_iter().filter(|&v| v == 2).count() == 2 {
-            //println!("Two pair {:?}", hand);
+    } else if card_counts.contains(&2)
+        || (num_jokers == 2)
+        || (num_jokers == 1 && card_counts.contains(&1))
+    {
+        if card_counts.iter().filter(|&v| *v == 2).count() == 2
+            || (card_counts.contains(&2) && num_jokers == 2)
+            || (card_counts.contains(&2) && num_jokers == 1 && card_counts.contains(&1))
+        {
             return 3;
         } else {
-            //println!("One pair {:?}", hand);
             return 2;
         }
     } else {
-        //println!("High card {:?}", hand);
         return 1;
     }
 }
 
 fn main() {
-    let data = read_input();
-    part1(&data);
+    let (data, data_joker) = read_input();
+    partx(&data, 1);
+    partx(&data_joker, 2);
 }
 
-fn read_input() -> Vec<CamelHand> {
+fn read_input() -> (Vec<CamelHand>, Vec<CamelHand>) {
     let contents = fs::read_to_string("D:\\Projects\\Code\\adventofcode\\2023\\day-07\\input.txt")
         .expect("Error reading input file");
 
     let mut hands: Vec<CamelHand> = Vec::new();
+    let mut hands_joker: Vec<CamelHand> = Vec::new();
 
     for line in contents.lines() {
         let mut split_line = line.split_ascii_whitespace();
-        let hand_str = split_line
-            .next()
-            .expect("Error getting hand string")
-            .trim()
-            .to_owned();
+        let hand_str = split_line.next().expect("Error getting hand string").trim();
         let mut hand_nums = [0; 5];
         for (c, i) in hand_str.chars().zip(hand_nums.iter_mut()) {
             *i = card_to_num(&c);
+        }
+        let mut hand_nums_joker = [0; 5];
+        for (c, i) in hand_str.chars().zip(hand_nums_joker.iter_mut()) {
+            *i = card_to_num_joker(&c);
         }
         let bid = split_line
             .next()
@@ -93,24 +120,31 @@ fn read_input() -> Vec<CamelHand> {
             .expect("Error parsing score number");
 
         let class = classify_hand(&hand_nums);
+        let class_joker = classify_hand(&hand_nums_joker);
 
         hands.push(CamelHand {
-            hand_str,
-            hand_nums,
             class,
+            hand_nums,
             bid,
         });
+        hands_joker.push(CamelHand {
+            class: class_joker,
+            hand_nums: hand_nums_joker,
+            bid,
+        })
     }
 
     hands.sort();
+    hands_joker.sort();
 
-    hands
+    (hands, hands_joker)
 }
 
-fn part1(hands: &Vec<CamelHand>) {
-    let mut total_winnings = 0;
-    for (i, hand) in hands.into_iter().enumerate() {
-        total_winnings += hand.bid * (i64::try_from(i).expect("Erro converting index to int") + 1);
-    }
-    println!("Part 1: {}", total_winnings);
+fn partx(hands: &Vec<CamelHand>, part: i64) {
+    let total_winnings = hands
+        .iter()
+        .enumerate()
+        .map(|t| t.1.bid * (i64::try_from(t.0).expect("Error converting index to int") + 1))
+        .fold(0, |a, b| a + b);
+    println!("Part {}: {}", part, total_winnings);
 }
