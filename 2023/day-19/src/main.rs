c913441f6d8785ddf3c53b262cea8279015fd183
skybax <tmflame@gmail.com>
2023-12-19 20:48:04 -0600

    day 19 part 2 not working

--- 2023/day-19/src/main.rs
+++ 2023/day-19/src/main.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, fs};
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 enum PartCateg {
     X,
     M,
@@ -8,7 +8,7 @@ enum PartCateg {
     S,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum CompType {
     LessThan,
     GreaterThan,
@@ -23,13 +23,14 @@ enum WorkflowRule {
 fn main() {
     let (workflows, parts) = read_input();
     part1(&workflows, &parts);
+    part2(&workflows);
 }
 
 fn read_input() -> (
     HashMap<String, Vec<WorkflowRule>>,
     Vec<HashMap<PartCateg, i64>>,
 ) {
-    let contents = fs::read_to_string("D:\\Projects\\Code\\adventofcode\\2023\\day-19\\input.txt")
+    let contents = fs::read_to_string("D:\\Projects\\Code\\adventofcode\\2023\\day-19\\example.txt")
         .expect("Error reading input file");
 
     let mut workflows = HashMap::new();
@@ -137,4 +138,97 @@ fn part1(workflows: &HashMap<String, Vec<WorkflowRule>>, parts: &Vec<HashMap<Par
         }
     }
     println!("Part 1: {}", total_value);
+}
+
+fn part2(workflows: &HashMap<String, Vec<WorkflowRule>>) {
+    // Need to build ranges of acceptable values
+    let next_workflow = &String::from("in");
+    let limits = Vec::new();
+    let paths = investigate_workflow(workflows, next_workflow, limits);
+    let mut total = 0;
+    for path in &paths {
+        let mut path_ranges = HashMap::new();
+        path_ranges.insert(PartCateg::X, (1, 4000));
+        path_ranges.insert(PartCateg::M, (1, 4000));
+        path_ranges.insert(PartCateg::A, (1, 4000));
+        path_ranges.insert(PartCateg::S, (1, 4000));
+        println!("{:?}", path);
+        for (part, comp, val) in path {
+            let range = path_ranges.get_mut(part).expect("Error getting part range");
+            match comp {
+                CompType::GreaterThan => { range.0 = range.0.max(*val); },
+                CompType::LessThan => { range.1 = range.1.min(*val); },
+            }
+        }
+        println!("{:?}", path_ranges);
+        let mut combinations = -1;
+        for part in [PartCateg::X, PartCateg::M, PartCateg::A, PartCateg::S] {
+            let path_range = path_ranges.get(&part).expect("Error getting path range");
+            if combinations < 0 {
+                combinations = path_range.1 - path_range.0 + 1;
+            } else {
+                combinations *= path_range.1 - path_range.0 + 1;
+            }
+        }
+        println!("{}", combinations);
+        total += combinations;
+    }
+    println!("{}", total);
+    // 97457328000000
+    // 93205398120000
+    // 167409079868000
+    // 256000000000000
+}
+
+fn investigate_workflow(workflows: &HashMap<String, Vec<WorkflowRule>>, workflow_name: &String, limits: Vec<(PartCateg, CompType, i64)>) -> Vec<Vec<(PartCateg, CompType, i64)>> {
+    let mut all_limits = Vec::new();
+    let mut local_limits = limits;
+    if let Some(workflow) = workflows.get(workflow_name) {
+        for rule in workflow {
+            match rule {
+                WorkflowRule::ConditionRule(
+                    part_category,
+                    comparison,
+                    val,
+                    val_if_true,
+                ) => {
+                    match comparison {
+                        CompType::GreaterThan => {
+                            let mut limits_clone = local_limits.clone();
+                            limits_clone.push((*part_category, *comparison, *val + 1));
+                            if val_if_true == "A" {
+                                all_limits.push(limits_clone);
+                                return all_limits;
+                            }
+                            let invest = investigate_workflow(workflows, val_if_true, limits_clone);
+                            all_limits.extend(invest);
+                            local_limits.push((*part_category, CompType::LessThan, *val));
+                        },
+                        CompType::LessThan => {
+                            let mut limits_clone = local_limits.clone();
+                            limits_clone.push((*part_category, *comparison, *val - 1));
+                            if val_if_true == "A" {
+                                all_limits.push(limits_clone);
+                                return all_limits;
+                            }
+                            let invest = investigate_workflow(workflows, val_if_true, limits_clone);
+                            all_limits.extend(invest);
+                            local_limits.push((*part_category, CompType::GreaterThan, *val));
+                        },
+                    }
+                }
+                WorkflowRule::FallThruRule(val_automatically) => {
+                    if val_automatically == "A" {
+                        all_limits.push(local_limits);
+                        return all_limits;
+                    }
+                    let invest = investigate_workflow(workflows, val_automatically, local_limits);
+                    all_limits.extend(invest);
+                    break;
+                }
+            }
+        }
+    }
+
+    return all_limits;
 }
